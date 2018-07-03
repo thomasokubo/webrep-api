80b3cead48ed1494691f55cf141d5f2594d90eee
Thomas <tai.okubo93@gmail.com>
2018-07-03 20:16:30 -0300

    Initial commit

---
+++ src/main/java/com/repnation/webrep/WebrepApplication.java
@@ -0,0 +1,12 @@
+package com.repnation.webrep;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebrepApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebrepApplication.class, args);
+	}
+}
