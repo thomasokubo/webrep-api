b741e8690e0038d90ae5a5299cb5499c2c4f15f9
Thomas <tai.okubo93@gmail.com>
2018-07-05 23:05:30 -0300

    Remove unnecessary imports

--- src/main/java/com/repnation/webrep/WebrepApplication.java
+++ src/main/java/com/repnation/webrep/WebrepApplication.java
@@ -1,13 +1,7 @@
 package com.repnation.webrep;
 
-import com.repnation.webrep.domain.model.User;
-import com.repnation.webrep.domain.repository.UserRepository;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-
-import java.util.Arrays;
 
 @SpringBootApplication
 public class WebrepApplication {
