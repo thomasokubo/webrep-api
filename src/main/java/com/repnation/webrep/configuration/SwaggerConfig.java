a3c797f7d818f8fe1ea2bb59e3f6fe6bd8491fdc
Thomas <tai.okubo93@gmail.com>
2018-07-18 22:40:52 -0300

    Add meta information to swagger page

--- src/main/java/com/repnation/webrep/configuration/SwaggerConfig.java
+++ src/main/java/com/repnation/webrep/configuration/SwaggerConfig.java
@@ -4,8 +4,11 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
@@ -20,6 +23,15 @@ public class SwaggerConfig extends WebMvcConfigurationSupport{
                 .select()
                 .apis(RequestHandlerSelectors.any())
                 .paths(PathSelectors.any())
+                .build()
+                .apiInfo(metaData());
+    }
+
+    private ApiInfo metaData() {
+        return new ApiInfoBuilder()
+                .title("WebRep API")
+                .description("\"A REST API to manage your lifestyle\"")
+                .contact(new Contact("Thomas Okubo", "thomasokubo.github.io", "tai.okubo93@gmail.com"))
                 .build();
     }
 
