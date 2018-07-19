315bd52f362229b89c72e52d2e56e3d5ab917443
Thomas <tai.okubo93@gmail.com>
2018-07-18 22:13:32 -0300

    Update SwaggerConfiguration class
    The class must extend WebMvcConfiguration and override its method due some changes in Spring Boot 2 woth Swagger2

--- src/main/java/com/repnation/webrep/configuration/SwaggerConfig.java
+++ src/main/java/com/repnation/webrep/configuration/SwaggerConfig.java
@@ -2,6 +2,8 @@ package com.repnation.webrep.configuration;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.spi.DocumentationType;
@@ -10,7 +12,7 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @Configuration
 @EnableSwagger2
-public class SwaggerConfig {
+public class SwaggerConfig extends WebMvcConfigurationSupport{
 
     @Bean
     public Docket api() {
@@ -19,5 +21,14 @@ public class SwaggerConfig {
                 .apis(RequestHandlerSelectors.any())
                 .paths(PathSelectors.any())
                 .build();
+    }
+
+    @Override
+    protected void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("swagger-ui.html")
+                .addResourceLocations("classpath:/META-INF/resources/");
+
+        registry.addResourceHandler("/webjars/**")
+                .addResourceLocations("classpath:/META-INF/resources/webjars/");
     }
 }
